25991700461806b076f78f2f2abb24cdc9505af8
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2023-12-13 10:44:21 +0800

    資料調整

---
+++ src/assets/js/voteData.js
@@ -0,0 +1,174 @@
+export const totalData = [
+  {
+    votePerson  : 19311105, // 選舉人數
+    validVotes  : 14300940, // 有效票數
+    invalidVotes: 163631,   // 無效票數
+    party_PFP   : 608590,   // 候選人得票
+    party_KMT   : 5522119,  // 候選人得票
+    party_PPT   : 8170231   // 候選人得票
+  }
+]
+
+export const countyVote = [
+  {
+    county      : 'tbs',
+    validVotes  : 1632453,
+    party_PPT   : 875854,
+    party_KMT   : 685830,
+    party_PFP   : 70769
+  },
+  {
+    county      : 'xbs',
+    validVotes  : 2466187,
+    party_PPT   : 1393936,
+    party_KMT   : 959631,
+    party_PFP   : 112620
+  },
+  {
+    county      : 'tys',
+    validVotes  : 1311141,
+    party_PPT   : 718260,
+    party_KMT   : 529749,
+    party_PFP   : 63132
+  },
+  {
+    county      : 'tzs',
+    validVotes  : 1698470,
+    party_PPT   : 967304,
+    party_KMT   : 646366,
+    party_PFP   : 84800
+  },
+  {
+    county      : 'tns',
+    validVotes  : 1167248,
+    party_PPT   : 786471,
+    party_KMT   : 339702,
+    party_PFP   : 41075
+  },
+  {
+    county      : 'gxs',
+    validVotes  : 1763826,
+    party_PPT   : 1097621,
+    party_KMT   : 610896,
+    party_PFP   : 55309
+  },
+  {
+    county      : 'xzx',
+    validVotes  : 325039,
+    party_PPT   : 152380,
+    party_KMT   : 154224,
+    party_PFP   : 18435
+  },
+  {
+    county      : 'mlx',
+    validVotes  : 326601,
+    party_PPT   : 147034,
+    party_KMT   : 164345,
+    party_PFP   : 15222
+  },
+  {
+    county      : 'zhx',
+    validVotes  : 763231,
+    party_PPT   : 436336,
+    party_KMT   : 291835,
+    party_PFP   : 35060
+  },
+  {
+    county      : 'ntx',
+    validVotes  : 299152,
+    party_PPT   : 152046,
+    party_KMT   : 133791,
+    party_PFP   : 13315
+  },
+  {
+    county      : 'yulx',
+    validVotes  : 399788,
+    party_PPT   : 246116,
+    party_KMT   : 138341,
+    party_PFP   : 15331
+  },
+  {
+    county      : 'jyx',
+    validVotes  : 307290,
+    party_PPT   : 197342,
+    party_KMT   : 98810,
+    party_PFP   : 11138
+  },
+  {
+    county      : 'pdx',
+    validVotes  : 511050,
+    party_PPT   : 317676,
+    party_KMT   : 179353,
+    party_PFP   : 14021
+  },
+  {
+    county      : 'ylx',
+    validVotes  : 274406,
+    party_PPT   : 173657,
+    party_KMT   : 90010,
+    party_PFP   : 10739
+  },
+  {
+    county      : 'hlx',
+    validVotes  : 185212,
+    party_PPT   : 66509,
+    party_KMT   : 111834,
+    party_PFP   : 6869
+  },
+  {
+    county      : 'tdx',
+    validVotes  : 115668,
+    party_PPT   : 44092,
+    party_KMT   : 67413,
+    party_PFP   : 4163
+  },
+  {
+    county      : 'phx',
+    validVotes  : 50904,
+    party_PPT   : 27410,
+    party_KMT   : 20911,
+    party_PFP   : 2583
+  },
+  {
+    county      : 'jls',
+    validVotes  : 226204,
+    party_PPT   : 114966,
+    party_KMT   : 99360,
+    party_PFP   : 11878
+  },
+  {
+    county      : 'xzs',
+    validVotes  : 261102,
+    party_PPT   : 144274,
+    party_KMT   : 102725,
+    party_PFP   : 14103
+  },
+  {
+    county      : 'jys',
+    validVotes  : 161738,
+    party_PPT   : 99265,
+    party_KMT   : 56269,
+    party_PFP   : 6204
+  },
+  {
+    county      : 'jmx',
+    validVotes  : 48040,
+    party_PPT   : 10456,
+    party_KMT   : 35948,
+    party_PFP   : 1636
+  },
+  {
+    county      : 'jmx',
+    validVotes  : 48040,
+    party_PPT   : 10456,
+    party_KMT   : 35948,
+    party_PFP   : 1636
+  },
+  {
+    county      : 'ljx',
+    validVotes  : 6190,
+    party_PPT   : 1226,
+    party_KMT   : 4776,
+    party_PFP   : 188
+  }
+]
